60cadd78365861d18876994f4d01b30288e1946d
JoudiTafran <jouditafran271@gmail.com>
2025-10-18 19:04:52 +0300

    Modify Side bar

--- src/app/layouts/sidebar/menu.ts
+++ src/app/layouts/sidebar/menu.ts
@@ -18,5 +18,23 @@ export const MENU: MenuItem[] = [
         label: 'MENUITEMS.APPS.LIST.SUBSCRIPTIONS',
         icon: 'ph-credit-card',
         link: '/apps/subscription'
+    },
+    {
+        id: 4,
+        label: 'MENUITEMS.APPS.LIST.TICKETS',
+        icon: 'ph-ticket',
+        link: '/apps/ticket'
+    },
+    {
+        id: 5,
+        label: 'MENUITEMS.APPS.LIST.TICKETS_ADMIN',
+        icon: 'ph-clipboard-text',
+        link: '/apps/ticket-admin'
+    },
+    {
+        id: 6,
+        label: 'MENUITEMS.APPS.LIST.TICKETS_USER',
+        icon: 'ph-user-circle',
+        link: '/apps/ticket-user'
     }
 ]
